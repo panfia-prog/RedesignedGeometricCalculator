11fe9e0594b0a34715e2eaca017a69d390ad9577
Nina <nina_gonzalez@dlsu.edu.ph>
2026-06-16 13:24:34 +0800

    finished rectangle class

--- Calculator/src/Rectangle.java
+++ Calculator/src/Rectangle.java
@@ -1,5 +1,29 @@
-public class Rectangle extends Shape{
-    public Rectangle(String color) {
-        super(color); //alternative to this.color and calls Shape
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        super("Default color");
+        this.width = width;
+        this.height = height;
     }
+
+    @Override
+    public double calculateArea(){
+        return width * height;
+    }
+
+    @Override
+    public double calculatePerimeter(){
+        return 2 * (width + height);
+
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + String.format(", Width: %.2f, Height: %.2f", width, height);
+
+    }
+
+
 }
