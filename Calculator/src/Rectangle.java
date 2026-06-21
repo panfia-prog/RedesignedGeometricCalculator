479f920847975624bec148b6c069626f9e311b56
panfia-prog <sofiasantostomas@gmail.com>
2026-06-21 12:51:05 +0800

    Updated Rectangle and Square classes

--- Calculator/src/Rectangle.java
+++ Calculator/src/Rectangle.java
@@ -1,9 +1,9 @@
-public class Rectangle extends Shape {
+public class Rectangle extends Shape implements Measurable2D{
     private double width;
     private double height;
 
-    public Rectangle(double width, double height) {
-        super("Default color");
+    public Rectangle(double width, double height, String color) {
+        super(color);
         this.width = width;
         this.height = height;
     }
@@ -12,15 +12,11 @@ public class Rectangle extends Shape {
         return width * height;
     }
 
-    public double calculatePerimeter(){
-        return 2 * (width + height);
-
-    }
+    public double calculatePerimeter(){return 2 * (width + height);}
 
     @Override
     public String toString(){
-        return super.toString() + String.format(", Width: %.2f, Height: %.2f", width, height);
-
+        return super.toString() + String.format("Rectangle Width: %.2f, Height: %.2f", width, height);
     }
 
 }
--- Calculator/src/Square.java
+++ Calculator/src/Square.java
@@ -1,7 +1,7 @@
 public class Square extends Rectangle{
     private double side;
-    public Square (double side){
-        super(side, side);
+    public Square (double side, String color){
+        super(side, side, color);
         this.side = side;
 
     }
@@ -18,9 +18,25 @@ public class Square extends Rectangle{
 
     @Override
     public String toString(){
-        return super.toString() + String.format(", Square Side: %.2f", side);
-
+        return String.format("Square Side: %.2f, Color %s", side, getColor());
     }
     }
 
+void main(){
+    double inputSide = Double.parseDouble(IO.readln("Enter the side: "));
+    double inputWidth = Double.parseDouble(IO.readln("Enter the width: "));
+    double inputHeight = Double.parseDouble(IO.readln("Enter the height: "));
+    String inputColor = IO.readln("Enter the color: ");
+    String inputColor2 = IO.readln("Enter the color: ");
+    Square shape = new Square(inputSide, inputColor);
+    Rectangle rectangle = new Rectangle(inputWidth,inputHeight,inputColor2);
+    IO.println("Square Area: " + shape.calculateArea());
+    IO.println("Square Perimeter " + shape.calculatePerimeter());
+    IO.println("Square Color: " + shape.getColor());
+    IO.println("Rectangle Area: " + rectangle.calculateArea());
+    IO.println("Rectangle Perimeter " + rectangle.calculatePerimeter());
+    IO.println("Rectangle Color: " + rectangle.getColor());
+    IO.println(shape.toString());
+    IO.println(rectangle.toString());
+}
 
